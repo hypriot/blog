e535855f82bb814739e6c420d38be142f90a3477
Stefan Scherer <scherer_stefan@icloud.com>
2018-03-07 14:06:07 +0100

    Switch Circle 2.0

--- ci-install-hugo.sh
+++ ci-install-hugo.sh
@@ -6,5 +6,5 @@ set -e
 # Install Hugo if not already cached or upgrade an old version.
 wget https://github.com/spf13/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz
 tar xvzf hugo_${HUGO_VERSION}_Linux-64bit.tar.gz
-cp hugo_${HUGO_VERSION}_linux_amd64/hugo_${HUGO_VERSION}_linux_amd64 $HOME/bin/hugo
+cp hugo_${HUGO_VERSION}_linux_amd64/hugo_${HUGO_VERSION}_linux_amd64 /usr/bin/hugo
 rm -rf hugo_${HUGO_VERSION}_linux_amd64*
